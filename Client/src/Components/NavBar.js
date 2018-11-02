dc492e619035745106187933edadb51af1bad9eb
Kristi Marks <ZG5804@engie.com>
2018-11-02 15:51:21 -0700

    fixing navbar"

--- Client/src/Components/NavBar.js
+++ Client/src/Components/NavBar.js
@@ -6,20 +6,9 @@ class Navbar extends Component{
         return (
             <nav className="navbar navbar-expand-xl navbar-dark bg-dark">
                 <a className="navbar-brand" href="#">Kristi_the_coder</a>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample06" aria-controls="navbarsExample06" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-      
-                <div className="collapse navbar-collapse" id="navbarsExample06">
-              <ul className="navbar-nav mr-auto">
-                <li className="nav-item active">
-                  <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">About</a>
-                </li>
                
-              </ul>
+                <div className="collapse navbar-collapse" id="navbarsExample06">
+             
              
             </div>
           </nav> 
